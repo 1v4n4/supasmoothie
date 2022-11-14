476fbcb2f7053857329788f30f8a013cc8fe84cb
1v4n4 <ivananl@yahoo.com>
2022-11-14 17:20:58 +0100

    Add order by attributes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import supabase from "../config/supaClient";
 const Home = () => {
   const [smoothies, setSmoothies] = useState(null);
   const [fetchError, setFetchError] = useState(null);
+  const [order, setOrder] = useState("created_at")
 
   const handleDelete = (id) => {
     setSmoothies(prevSmoothies => {
@@ -16,7 +17,8 @@ const Home = () => {
     const fetchSmoothies = async () => {
       const { data, error } = await supabase
         .from("smoothies")
-        .select();
+        .select()
+        .order(order, { asceding: false })
 
       if (error) {
         setFetchError("Oops, something went wrong. Can't get smoothies");
@@ -30,12 +32,19 @@ const Home = () => {
 
     fetchSmoothies();
 
-}, [])
+  }, [order])
+
   return (
     <div className="page home">
       {fetchError && <p>{fetchError}</p>}
       {smoothies &&
         (<div className="smoothies">
+         <div className="order-by">
+            <p>Order by:</p>
+            <button onClick={() => setOrder('created_at')}>Created</button>
+            <button onClick={() => setOrder('title')}>Title</button>
+            <button onClick={() => setOrder('rating')}>Rating</button>
+          </div>
         <div className="smoothie-grid">
           {smoothies.map(smoothie =>
             <SmoothieCard
