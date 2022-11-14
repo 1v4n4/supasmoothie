6661948781c6d7332cbe31a0ece0e48940687f21
1v4n4 <ivananl@yahoo.com>
2022-11-14 14:54:51 +0100

    Fetch smoothies from supabase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,46 @@
+import React, { useEffect, useState }  from 'react';
+import Smoothie from '../components/Smoothie';
 import supabase from "../config/supaClient";
 
 const Home = () => {
+  const [smoothies, setSmoothies] = useState(null);
+  const [fetchError, setFetchError] = useState(null);
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+        .from("smoothies")
+        .select();
+
+      if (error) {
+        setFetchError("Oops, something went wrong. Can't get smoothies");
+        setSmoothies(null);
+      }
+      if (data) {
+        setSmoothies(data.smoothies);
+        setFetchError(null);
+      }
+    }
+
+    fetchSmoothies();
+
+}, [])
+
+
+
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p>fetchError</p>}
+      {smoothies &&
+        (<div className="smoothies">
+          {smoothies.map(smoothie => <Smoothie smoothie={smoothie} />)}
+
+        </div>)
+      }
     </div>
-  )
+
+  );
 }
 
 export default Home
