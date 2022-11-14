ada7f9132e029464fd599a1c9b5a41381eb6d947
1v4n4 <ivananl@yahoo.com>
2022-11-14 16:11:26 +0100

    Add link to update page

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SmoothieCard = ({ smoothie }) => {
   console.log(smoothie);
@@ -7,6 +8,9 @@ const SmoothieCard = ({ smoothie }) => {
       <h3>{smoothie.title}</h3>
       <p>{smoothie.method}</p>
       <div className='rating'>{smoothie.rating}</div>
+      <Link to={'/' + smoothie.id}>
+      <i className="material-icons">edit</i>
+      </Link>
     </div>
   )
 }
